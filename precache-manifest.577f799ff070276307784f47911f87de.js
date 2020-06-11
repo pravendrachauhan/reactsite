57acf12e8cd3d0da0f7a3913b2694c277912ec0d
pravendra.chauhan <praveen.07022016@gmail.com>
2020-06-11 21:30:23 +0530

    Updates

---
+++ precache-manifest.577f799ff070276307784f47911f87de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ed1b2f8c51adc99bd6e8c14ac7b4e7",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "1cd91cc53d01a24e12d0",
+    "url": "/react/static/css/main.54047d4e.chunk.css"
+  },
+  {
+    "revision": "8b73bf04cabebe897222",
+    "url": "/react/static/js/2.d924c651.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react/static/js/2.d924c651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd91cc53d01a24e12d0",
+    "url": "/react/static/js/main.79bfdc7e.chunk.js"
+  },
+  {
+    "revision": "fe9bea6164fe389688c4",
+    "url": "/react/static/js/runtime-main.55944e32.js"
+  },
+  {
+    "revision": "aefa5817abb71c53bc025704f2bae1f7",
+    "url": "/react/static/media/Image1.aefa5817.jpg"
+  },
+  {
+    "revision": "ebcdcc3de53c585cdd6ff60e98c00793",
+    "url": "/react/static/media/Image2.ebcdcc3d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd07bc09983f8632ced1c38a46b80781",
+    "url": "/react/static/media/tech-support.fd07bc09.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67905908c7c78416fcb5abbc2b0e7305.js"
+  "/react/precache-manifest.577f799ff070276307784f47911f87de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
